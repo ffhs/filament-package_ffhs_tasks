2b5787760ae987b03db0b86a38cfa83e33f3902d
Dennis Koch <info@pixelarbeit.de>
2026-01-25 21:28:16 +0100

    Fix `UserFactory` namespace import and docblock reference

--- workbench/app/Models/User.php
+++ workbench/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Ffhs\FfhsTasks\Database\Factories\UserFactory;
+use Database\Factories\UserFactory;
 use Ffhs\FfhsTasks\Traits\IsTaskUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
     use Notifiable;
     use IsTaskUser;
