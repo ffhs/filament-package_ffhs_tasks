a6ec874416045560ec3e09c53008fa647c2924e4
Odion-DS <93391784+Odion-DS@users.noreply.github.com>
2026-04-02 14:54:51 +0000

    Code style

--- src/Filament/Resources/Tasks/Pages/CreateTask.php
+++ src/Filament/Resources/Tasks/Pages/CreateTask.php
@@ -3,7 +3,6 @@
 namespace Ffhs\FfhsTasks\Filament\Resources\Tasks\Pages;
 
 use Ffhs\FfhsTasks\Filament\Resources\Tasks\Schemas\TaskForm;
-use Ffhs\FfhsTasks\Filament\Resources\Tasks\Schemas\TaskGroupForm;
 use Ffhs\FfhsTasks\Filament\Resources\Tasks\TaskResource;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Schemas\Schema;
