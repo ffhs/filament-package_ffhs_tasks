7d42643cf5484fb6c7b577c4e41e0cb11ff977c4
Luc Kromer <luc.kromer@ffhs.ch>
2026-04-02 16:41:59 +0200

    -_-

--- src/Filament/Resources/Tasks/Pages/EditTask.php
+++ src/Filament/Resources/Tasks/Pages/EditTask.php
@@ -3,6 +3,7 @@
 namespace Ffhs\FfhsTasks\Filament\Resources\Tasks\Pages;
 
 use Ffhs\FfhsTasks\Filament\Resources\Tasks\Actions\HandleAction;
+use Ffhs\FfhsTasks\Filament\Resources\Tasks\Schemas\TaskForm;
 use Ffhs\FfhsTasks\Filament\Resources\Tasks\Schemas\TaskGroupForm;
 use Ffhs\FfhsTasks\Filament\Resources\Tasks\TaskResource;
 use Ffhs\FfhsTasks\Models\Task;
@@ -41,7 +42,7 @@ class EditTask extends EditRecord
 
     public function form(Schema $schema): Schema
     {
-        return TaskGroupForm::configure($schema, $this);
+        return TaskForm::configure($schema, $this);
     }
 
     public function afterSave(): void
